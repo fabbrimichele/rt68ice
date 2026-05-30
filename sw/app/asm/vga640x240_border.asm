214c67b94dccece97ab3d14f52965dc03766163a
Michele Fabbri <fabbrimichele75@gmail.com>
2026-05-30 14:21:00 +0200

    VGA 640x200 4bpp: fix colors

--- sw/app/asm/vga640x240_border.asm
+++ sw/app/asm/vga640x240_border.asm
@@ -15,35 +15,40 @@ draw_bands:
     lea     (_fb_start+(LINE_WIDTH_B*90)),a0    ; starts at line 90
 
     ; Green band
-    move.l  #$FFFF0000,d1                   ; Green
-    move.w  #29,d2                          ; 30 horizonatl lines (-1 for dbra)
+    move.l  #$0000FFFF,d1                       ; Color 2 -> green
+    move.l  #$00000000,d2                       ;
+    move.w  #29,d3                              ; 30 horizonatl lines (-1 for dbra)
 .green_loop:
     bsr     hline
-    dbra    d2,.green_loop
+    dbra    d3,.green_loop
 
     ; Red band
-    move.l  #$0000FFFF,d1                   ; Red
-    move.w  #29,d2                          ; 30 horizonatl lines (-1 for dbra)
+    move.l  #$00000000,d1                       ; Color 4 -> red
+    move.l  #$FFFF0000,d2                       ;
+    move.w  #29,d3                              ; 30 horizonatl lines (-1 for dbra)
 .red_loop:
     bsr     hline
-    dbra    d2,.red_loop
+    dbra    d3,.red_loop
 
     rts
 
 ; Draw white border
 draw_border:
     ; Horizontal lines
-    move.l  #$FFFFFFFF,d1
+    move.l  #$FFFFFFFF,d1                       ; Color 15 -> white
+    move.l  #$FFFFFFFF,d2                       ; and all pixels on
     lea     _fb_start,a0                        ; First line
     bsr     hline
     lea     (_fb_start+(239*LINE_WIDTH_B)),a0   ; Last line (in bytes)
     bsr     hline
     ; Vertical lines
     move.b  #0,LED
-    move.l  #$80008000,d1
+    move.l  #$80008000,d1                       ; Color 15 -> white
+    move.l  #$80008000,d2                       ; most left pixel
     lea     _fb_start,a0
     bsr     vline
-    move.l  #$00010001,d1
+    move.l  #$00010001,d1                       ; Color 15 -> white
+    move.l  #$00010001,d2                       ; most right pixel
     lea     (_fb_start+LINE_WIDTH_B-8),a0
     bsr     vline
     rts
@@ -59,22 +64,25 @@ clr_screen:
 
 ; Draw a full horizontal line
 ; Input: a0 starting address
-;        d1.w pattern
+;        d1.l planes 0 and 1
+;        d2.l planes 2 and 3
 hline:
-    move.w  #79,d0
+    move.w  #39,d0
 .loop:
-    move.l  d1,(a0)+                ; Draw 16 white pixels (2 interleaved bitplanes -> 32 bits)
+    move.l  d1,(a0)+                ; Draw 16 white pixels (4 interleaved planes)
+    move.l  d2,(a0)+                ;
     dbra    d0,.loop
     rts
 
 ; Draw a full vertical line
 ; Input: a0   starting address
-;        d1.w pattern
+;        d1.l planes 0 and 1
+;        d2.l planes 2 and 3
 vline:
     move.w  #239,d0                 ; 240 - 1 for dbra
 .loop:
     or.l    d1,(a0)
-    or.l    d1,4(a0)
+    or.l    d2,4(a0)
     add.l   #LINE_WIDTH_B,a0
     dbra    d0,.loop
     rts
